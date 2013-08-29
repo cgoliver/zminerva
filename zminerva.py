4d1da0703274b78fb7a14dda48623c53b3e2836e
zulban <zulban@gmail.com>
2013-08-29 11:40:39 -0400

    changed version number for first time, i guess

--- zminerva.py
+++ zminerva.py
@@ -119,7 +119,7 @@ def main(args):
                 args=args)
 
 if __name__ == "__main__":
-    args = docopt(__doc__, version="0.1")
+    args = docopt(__doc__, version="1.0")
     main(args)
 
 
